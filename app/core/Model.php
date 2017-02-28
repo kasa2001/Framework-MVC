280bab6db6066c38d5b9c7a8ab1a0b45769daf24
sakso <pawelgomolka@interia.pl>
2017-02-28 18:37:58 +0000

    Framework project

--- app/core/Model.php
+++ app/core/Model.php
@@ -6,7 +6,8 @@ class Model extends Database {
     public function __construct()
     {
         /*
-         * Function in test. If you download this framework change inquiry theregit remote add origin https://github.com/kasa2001/projects.git
+         * Function in test. If you download this framework change inquiry there
+         *
          * */
         $this->connection = new Database("projekt");
         $this->data = $this->connection->request($this->connection->connect,"SELECT * FROM `users` WHERE `Nick` = 'Marianek'");
