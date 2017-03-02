978033974d579d11806da860b8d70e5e0de579c7
sakso <pawelgomolka@interia.pl>
2017-03-02 11:28:43 +0000

    Optimization part one

--- app/core/Database.php
+++ app/core/Database.php
@@ -30,30 +30,85 @@ class Database
     }
 
     /**
-     * Function which send query to database and get result query
-     * @param $connect Database data about connection
-     * @return object array (return score of $query)
-     * */
-    public function request($connect)
+     * Function which create a new query
+     * @param $table string data about load table
+     * @param $choose integer select type of query (1 SELECT, 2 INSERT, 3 DELETE, 4 UPDATE)
+     * @param $modify integer degree modify the query (implicitly 0)
+     * @param $data (table string) additional data (implicitly empty table)
+     * @param $sort integer sort score query (implicitly 0)
+     * @return string return generated query
+     */
+    public function createQuery($table, $choose, $modify = 0, $data = [], $sort = 0)
     {
-        return $connect->query($this->query);
+        switch ($choose) {
+            case 1:
+                return $this->createSelectQuery($table, $modify, $data, $sort);
+            case 2:
+                return $this->createInsertQuery($table, $data);
+            case 3:
+                return $this->createDeleteQuery($table, $modify, $data);
+            case 4:
+                return $this->createUpdateQuery($table, $modify, $data);
+            default:
+                echo 'Bad choose query. Check second param in call method createQuery()';
+        }
+        return NULL;
     }
 
-    public function where($query, $i, $n, $data, $modify)
+    public function createSelectQuery($table, $modify, $data = [], $sort)
     {
+        $query = "SELECT * FROM `" . $table . "` ";
+        if ($modify) {
+            $sort != 0? $i = 1:$i = 0;
+            $n = count($data);
+            if ($i < $n) $query = $this->where($query, $i, $n, $data, $modify);
+            else if ($i > $n) return $this->warning();
+        }
+        if ($sort) $query=$this->sort($query,$data,$sort);
+        return $query;
+    }
+
+    public function sort($query, $data, $sort){
+        $query .= "ORDER BY `" . $data[0];
+        $sort == 1? $query .= "` ASC":$query .= "` DESC";
+        return $query;
+    }
+
+    public function createDeleteQuery($table, $modify, $data = [])
+    {
+        $query = "DELETE FROM `" . $table . "` ";
+        $i = 0;
+        $n = count($data);
+        if ($i > $n) return $this->warning();
+        $query = $this->where($query, $i, $n, $data, $modify);
+        return $query;
+    }
+
+    public function createUpdateQuery($table, $modify, $data)
+    {
+        $query = "UPDATE `" . $table . "` SET `" . $data[0] . "` = '" . $data[1] . "' ";
+        $query = $this->where($query, 2, count($data), $data, $modify);
+        return $query;
+    }
+
+    /**
+     * Function which add to query WHERE
+     * @param $query string. It is a query
+     * @param $i integer. It is a control param
+     * @param $n integer. It is a size of array $data
+     * @param $data array string. Is is a data to transfer to query
+     * @param $modify
+     * @return string $query
+     * */
+    public function where($query, $i, $n, $data = [], $modify)
+    {
+        $query .= "WHERE `";
         for (; $i < $n; $i += 2) {
-            if ($i + 1 == $n) {
-                echo 'Warning: Too small or big array in call function createQuery()';
-                return NULL;
-            } else {
+            if ($i + 1 == $n) return $this->warning();
+            else {
                 $query .= $data[$i] . "` = '" . $data[$i + 1] . "' ";
-                if (($i + 1) < ($n - 1)) {
-                    if ($modify == 1) {
-                        $query .= "AND `";
-                    } else if ($modify > 1) {
-                        $query .= "OR `";
-                    }
-                }
+                if (($i + 1) < ($n - 1) and $modify == 1) $query .= "AND `";
+                else if (($i + 1) < ($n - 1) and $modify > 1) $query .= "OR `";
             }
         }
         return $query;
@@ -86,12 +141,12 @@ class Database
                         }else if ($modify=="o"){
                             $table[$i + 1] = 'OR';
                         }
-                            $table[$i + 2] = "`" . $data[$index] . "`";
-                            $table[$i + 3] = "=";
-                            $table[$i + 4] = "'" . $data[$index + 1] . "'";
-                            unset($data[$index]);
-                            unset($data[$index + 1]);
-                            $index += 2;
+                        $table[$i + 2] = "`" . $data[$index] . "`";
+                        $table[$i + 3] = "=";
+                        $table[$i + 4] = "'" . $data[$index + 1] . "'";
+                        unset($data[$index]);
+                        unset($data[$index + 1]);
+                        $index += 2;
                     } else {
                         return implode(" ", $table);
                     }
@@ -112,100 +167,45 @@ class Database
             echo "Warning: param n is not even. Check data array in function createQuery()";
             return NULL;
         }
-        for ($i = 0; $i < ($n / 2); $i++) {
+        $query = $this->columns($query, 0, $n,$data);
+        $query .= ") VALUES (";
+        $query = $this->values($query, ($n/2), $n,$data);
+        $query .= ");";
+        return $query;
+    }
+
+    public function columns($query, $i, $n, $data=[]){
+        for (; $i < ($n / 2); $i++) {
             $query .= "`" . $data[$i] . "`";
             if (($i) != (($n / 2) - 1)) {
                 $query .= ", ";
             }
         }
-        $query .= ") VALUES (";
+        return $query;
+    }
+
+    public function values($query, $i, $n, $data=[]){
         for (; $i < $n; $i++) {
             $query .= "'" . $data[$i] . "'";
             if (($i) != ($n - 1)) {
                 $query .= ", ";
             }
         }
-        $query .= ");";
         return $query;
     }
 
-    public function createSelectQuery($table, $modify, $data = [], $sort)
-    {
-        $temp = 0;
-        $i = 0;
-        $query = "SELECT * FROM `" . $table . "` ";
-        if ($modify) {
-            if ($sort != 0) {
-                $temp = 1;
-                $i = 1;
-            }
-            $n = count($data);
-            if ($i < $n) {
-                $query .= "WHERE `";
-            } else if ($i > $n) {
-                echo 'Warning: Too small table in call function createQuery()';
-                return NULL;
-            }
-            $query = $this->where($query, $i, $n, $data, $modify);
-        }
-        if ($temp) {
-            $query .= "ORDER BY `" . $data[0];
-            if ($sort == 1) {
-                $query .= "` ASC";
-            } else if ($sort == 2) {
-                $query .= "` DESC";
-            }
-        }
-        return $query;
-    }
-
-    public function createDeleteQuery($table, $modify, $data = [])
-    {
-        $query = "DELETE FROM `" . $table . "` ";
-        $i = 0;
-        $n = count($data);
-        if ($i < $n) {
-            $query .= "WHERE `";
-        } else if ($i > $n) {
-            echo 'Warning: Too small or big array in call function createQuery()';
-            return NULL;
-        }
-        $query = $this->where($query, $i, $n, $data, $modify);
-        return $query;
-    }
-
-    public function createUpdateQuery($table, $modify, $data)
-    {
-        $query = "UPDATE `" . $table . "` SET `" . $data[0] . "` = '" . $data[1] . "' WHERE `";
-        $query = $this->where($query, 2, count($data), $data, $modify);
-        return $query;
+    public function warning(){
+        echo 'Warning: Too small array in call method createQuery()';
+        return NULL;
     }
 
     /**
-     * Function which create a new query
-     * @param $table string data about load table
-     * @param $choose integer select type of query (1 SELECT, 2 INSERT, 3 DELETE, 4 UPDATE)
-     * @param $modify integer degree modify the query (implicitly 0)
-     * @param $data (table string) additional data (implicitly empty table)
-     * @param $sort integer sort score query (implicitly 0)
-     * @return string return generated query
-     */
-    public function createQuery($table, $choose, $modify = 0, $data = [], $sort = 0)
+     * Function which send query to database and get result query
+     * @param $connect Database data about connection
+     * @return object array (return score of $query)
+     * */
+    public function request($connect)
     {
-        $query = NULL;
-        switch ($choose) {
-            case 1:
-                return $this->createSelectQuery($table, $modify, $data, $sort);
-            case 2:
-                return $this->createInsertQuery($table, $data);
-            case 3:
-                return $this->createDeleteQuery($table, $modify, $data);
-            case 4:
-                return $this->createUpdateQuery($table, $modify, $data);
-                break;
-            default:
-                echo 'Bad choose query. Check second param in call method createQuery()';
-        }
-        return $query;
+        return $connect->query($this->query);
     }
 }
